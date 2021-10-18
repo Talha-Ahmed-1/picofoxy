c0409637ed8a11e3275bfbda46ebde2b77c44b09
Talha Ahmed <talhaahmedashraf@gmail.com>
2021-10-18 10:43:20 +0500

    update masking

--- extraCheckpoints/src1/main/scala/jigsaw/peripherals/spiflash/Spi.scala
+++ extraCheckpoints/src1/main/scala/jigsaw/peripherals/spiflash/Spi.scala
@@ -1,12 +1,14 @@
 package jigsaw.peripherals.spiflash
 
+// import caravan.bus.common.{AddressMap, BusDecoder, DeviceAdapter, Switch1toN, DummyMemController, Peripherals} // imported DummyMemController
 import caravan.bus.common.{AbstrRequest, AbstrResponse}
+// import caravan.bus.tilelink._
 import chisel3._
 import chisel3.experimental.ChiselEnum
 import chisel3.stage.ChiselStage
-import chisel3.util.{Cat, Decoupled, Fill, MuxCase, Enum}
+import chisel3.util.{Cat, Decoupled, Fill}
 import jigsaw.peripherals.spi._
-// import jigsaw.peripherals.common._
+import jigsaw.peripherals.common._
 
 class Spi_IO[A <: AbstrRequest, B <: AbstrResponse]
           (gen: A, gen1: B) extends Bundle{
@@ -26,114 +28,70 @@ class Spi[A <: AbstrRequest, B <: AbstrResponse]
           (gen: A, gen1: B)(implicit val spiConfig: Config) extends Module{
 
     val io = IO(new Spi_IO(gen, gen1))
+    // val spiProtocol = Module(new Protocol())
     val ControlReg = RegInit(0.U(32.W))
-    val TxDataReg    = RegInit(0.U(32.W))
-    val RxDataReg    = RegInit(0.U(32.W))
-    val RxDataValidReg = RegInit(0.B)
 
     val vv = Mux(io.req.bits.addrRequest === 0.U, 0.B, 1.B)
-    val maskedData = Wire(Vec(4, UInt(8.W)))
-    maskedData := io.req.bits.activeByteLane.asTypeOf(Vec(4, Bool())) map (Fill(8,_))
-
-    val setControlStatusReg :: readControlStatusReg :: setTxDataReg :: readTxDataReg :: readRxDataReg :: Nil = Enum(5)
-    // val process = RegInit(0.U)
-
-    // val array = Array(
-        // (io.req.bits.addrRequest === 0.U && io.req.bits.isWrite === 1.B) -> setControlStatusReg,
-        // (io.req.bits.addrRequest === 0.U && io.req.bits.isWrite === 0.B) -> readControlStatusReg
-        // (io.req.bits.addrRequest === 3.U && io.req.bits.isWrite === 1.B) -> setTxDataReg
-        // (io.req.bits.addrRequest === 3.U && io.req.bits.isWrite === 0.B) -> readTxDataReg,
-        // (io.req.bits.addrRequest === 7.U && io.req.bits.isWrite === 0.B) -> readRxDataReg
-    // )
-
-    // process := MuxCase(setTxDataReg, array)
+    // val maskedData = Wire(Vec(4, UInt(8.W)))
 
     when (io.req.bits.addrRequest === 0.U && io.req.bits.isWrite === 1.B){
-        ControlReg := Mux(io.req.valid, io.req.bits.dataRequest & maskedData.asUInt, 0.U)
-
-        io.rsp.bits.dataResponse := Mux(io.rsp.ready, io.req.bits.addrRequest, 0.U)
-        io.rsp.bits.error := Mux(io.req.valid, 0.B, 1.B)
+        // val data = io.req.bits.dataRequest.asTypeOf(Vec(4, UInt(8.W)))
+        // maskedData := io.req.bits.activeByteLane.asBools zip data map { case (i:Bool, d:UInt) => Mux(i, d, 0.U)}
+    // val maskedData = io.req.bits.dataRequest.asTypeOf(Vec(4, UInt(8.W)))        // breaking into Vecs to apply masking
+    // val data = io.req.bits.activeByteLane zip maskedData map {                  // applying maskiing a/c to mask bits (activeByteLane)
+    //             case (b:Bool, i:UInt) => Mux(b, i, 0.U)
+    //     }
+        ControlReg := io.req.bits.dataRequest
         
+        // ControlReg := maskedData.asUInt
 
-        List(io.req.ready, io.rsp.valid) map (_ := 1.B)
-        List(io.cs_n, io.sclk, io.mosi) map (_ := DontCare)
-    }
-    .elsewhen(io.req.bits.addrRequest === 0.U && io.req.bits.isWrite === 0.B){
-        io.rsp.bits.dataResponse := Mux(io.rsp.ready, ControlReg, 0.U)
-        io.rsp.bits.error := Mux(io.req.valid, 0.B, 1.B)
-        
+        io.req.ready := 1.B
+        io.rsp.bits.dataResponse := io.req.bits.dataRequest
+        io.rsp.bits.error := 0.B
+        io.rsp.valid := 1.B
 
-        List(io.req.ready, io.rsp.valid) map (_ := 1.B)
-        List(io.cs_n, io.sclk, io.mosi) map (_ := DontCare) 
-    }
-    .elsewhen(io.req.bits.addrRequest === 3.U && io.req.bits.isWrite === 1.B){
-        TxDataReg := Mux(io.req.valid, Cat("b00000011".U,(io.req.bits.dataRequest & maskedData.asUInt)(23,0)), 0.U)
+        io.cs_n := DontCare
+        io.sclk := DontCare
+        io.mosi := DontCare
+    }.elsewhen(io.req.bits.addrRequest === 3.U && io.req.bits.isWrite === 1.B){
+        // val data = io.req.bits.dataRequest.asTypeOf(Vec(4, UInt(8.W)))
+        // maskedData := io.req.bits.activeByteLane.asBools zip data map { case (i:Bool, d:UInt) => Mux(i, d, 0.U)}
+    
+        val spiProtocol = Module(new Protocol())
+        // val data = io.req.bits.dataRequest & Cat(io.req.bits.activeByteLane.asBools map {case (_:Bool) => Mux(_ === 1.B, Fill(8, 1.B), Fill(8, 0.B))})
+        spiProtocol.io.data_in.bits  := io.req.bits.dataRequest
+        // spiProtocol.io.data_in.bits := maskedData.asUInt
+        spiProtocol.io.data_in.valid := vv
+        io.req.ready := spiProtocol.io.data_in.ready
 
-        io.rsp.bits.dataResponse := Mux(io.rsp.ready, io.req.bits.addrRequest, 0.U)
-        io.rsp.bits.error := Mux(io.req.valid, 0.B, 1.B)
-        
+        io.rsp.bits.dataResponse := spiProtocol.io.data_out.bits
+        io.rsp.valid := spiProtocol.io.data_out.valid
+        spiProtocol.io.data_out.ready := io.rsp.ready
+        io.rsp.bits.error := 0.B
 
-        List(io.req.ready, io.rsp.valid) map (_ := 1.B)
-        List(io.cs_n, io.sclk, io.mosi) map (_ := DontCare)
-    }
-    .elsewhen(io.req.bits.addrRequest === 3.U && io.req.bits.isWrite === 0.B){
-        io.rsp.bits.dataResponse := Mux(io.rsp.ready, TxDataReg, 0.U)
-        io.rsp.bits.error := Mux(io.req.valid, 0.B, 1.B)
-        
+        spiProtocol.io.resetProtocol := 0.B
+        spiProtocol.io.CPOL := ControlReg(1)
+        spiProtocol.io.CPHA := ControlReg(0)
+        spiProtocol.io.miso := io.miso
 
-        List(io.req.ready, io.rsp.valid) map (_ := 1.B)
-        List(io.cs_n, io.sclk, io.mosi) map (_ := DontCare)
-    }
-    .elsewhen(io.req.bits.addrRequest === 7.U && io.req.bits.isWrite === 0.B){
-        io.rsp.bits.dataResponse := Mux(io.rsp.ready, RxDataReg, 0.U)
-        io.rsp.bits.error := Mux(io.req.valid, 0.B, 1.B)
-        io.rsp.valid := RxDataValidReg
+        io.mosi := spiProtocol.io.mosi
+        io.sclk := spiProtocol.io.sck
+        io.cs_n := spiProtocol.io.ss
 
-        List(io.req.ready) map (_ := 1.B)
-        List(io.cs_n, io.sclk, io.mosi) map (_ := DontCare)
-    }
-    .otherwise{
-        List(io.req.ready, io.rsp.bits.error, io.rsp.valid) map (_ := 1.B)
-        List(io.cs_n, io.sclk, io.mosi) map (_ := DontCare)
+    }.otherwise{
+        io.req.ready := 1.B
         io.rsp.bits.dataResponse := io.req.bits.addrRequest
+        io.rsp.bits.error := 1.B
+        io.rsp.valid := 1.B
 
-        maskedData map (_ := DontCare)
+        io.cs_n := DontCare
+        io.sclk := DontCare
+        io.mosi := DontCare
     }
-
-    when(ControlReg(2)){
-    // val spiProtocol = withClock(clock) { Module(new Protocol())}
-    val spiProtocol = Module(new Protocol())
-    // spiProtocol.clock := clockGen(ControlReg(17,3)).asClock()
-    spiProtocol.io.data_in.bits  := TxDataReg// & maskedData.asUInt
-    spiProtocol.io.data_in.valid := ControlReg(2)
-    spiProtocol.io.CPOL := ControlReg(1)
-    spiProtocol.io.CPHA := ControlReg(0)
-    spiProtocol.io.miso := io.miso
-    spiProtocol.io.data_out.ready := 1.B
-    List(io.mosi, io.sclk, io.cs_n) zip List(spiProtocol.io.mosi, spiProtocol.io.sck, spiProtocol.io.ss) map (a => a._1 := a._2)
-
-    // RxDataReg := Mux(spiProtocol.io.data_out.valid, spiProtocol.io.data_out.bits, 0.U)
-    // RxDataReg := spiProtocol.io.data_out.bits
-    when(spiProtocol.io.data_out.valid){
-        RxDataReg := spiProtocol.io.data_out.bits
-        RxDataValidReg := 1.B
-    }
-    }
-
-    // def counter(max: UInt) = {
-    //     val x = RegInit(0.asUInt(max.getWidth.W))
-    //     x := Mux(x === max, 0.U, x + 1.U)
-    //     x
-    // }
-    // def pulse(n: UInt) = counter(n - 1.U) === 0.U
-    // def toggle(p: Bool) = {
-    //     val x = RegInit(false.B)
-    //     x := Mux(p, !x, x)
-    //     x
-    // }
-    // def clockGen(period: UInt) = toggle(pulse(period >> 1))
-
+    // maskedData map (_ := DontCare)
 }
+
+
 
 
 
--- fpga/program.asm
+++ fpga/program.asm
@@ -1,22 +1,5 @@
-li x15, 0x40001000
-li x14, 200000
-LOOP:
- addi x3,x3,0
- sw x3, 0x0(x15)
- li x5,0x1
- li x6,300000
- li x7,0
-LOOP1:
-sw x5, 0x3(x15)
- addi x7,x7,1
- bne x7,x6,LOOP1
- li x6,300000
- li x7,0
- li x5,0x0
-LOOP2:
- sw x5, 0x3(x15)
- addi x7,x7,1
- bne x7,x6,LOOP2
+li x15, 0x40001000 #base address
+li x7 , 0x3 #data regitser
+li x8 , 1048575 #data
 
-
- j LOOP
+sw x8 , 0x3(x15)
